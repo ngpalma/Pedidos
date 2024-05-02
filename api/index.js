2510abf24d6d6a8b06355e6975eacd739d8ba994
ngpalma <nicolasgerardopalma@gmail.com>
2024-05-01 21:00:16 -0300

    mensaje en consola de conexión exitosa de la base de datos

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
+  console.log("All models were synchronized successfully.");
   server.listen(port, () => {
     console.log(`Server listening at port ${port}`); // eslint-disable-line no-console
   });
